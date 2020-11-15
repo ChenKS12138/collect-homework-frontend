26e71850d9adeebd6ab22c98255fbf7aefd047e6
ChenKS12138 <749923710@qq.com>
2020-11-15 12:08:52 +0800

    feat: redux logger

--- src/utils/duck.tsx
+++ src/utils/duck.tsx
@@ -3,47 +3,21 @@ import { DuckMap, DuckOptions, INIT, END } from "saga-duck";
 import { parallel } from "redux-saga-catch";
 import { useReactSaga } from "use-react-saga";
 
-let createEnhanceDispatch = (dispatch, state) => dispatch;
-
-if (process.env.NODE_ENV !== "production") {
-  const { createLogger } = require("redux-logger");
-  const loggerMiddleware = createLogger({ collapsed: true });
-  createEnhanceDispatch = (dispatch, state) => {
-    return loggerMiddleware({ getState: () => state, dispatch })(dispatch);
-  };
-}
-
 export function useSagaDuckState<T extends DuckMap = any>(
   DM: new (options?: DuckOptions) => T
 ) {
   const duck = useMemo(() => {
     return new DM();
   }, []);
-  const initState = useMemo(() => {
-    const makeState = (obj) => {
-      const state = {};
-      for (const key in obj) {
-        state[key] = {};
-        for (const duckKey in obj[key]?.ducks) {
-          state[key][duckKey] = makeState(obj[key]?.ducks);
-        }
-      }
-      return state;
-    };
-    return makeState(duck.ducks);
-  }, []);
-  const init = useCallback((state) => {
-    return state;
-  }, []);
+
   const [state, dispatch]: [any, any] = useReducer(
-    duck.reducer as any,
-    initState as any,
-    init as any
+    logger(duck.reducer) as any,
+    duck.reducer(undefined, { type: "INIT" })
   );
 
   const enhanceDispatch = useReactSaga({
     state,
-    dispatch: createEnhanceDispatch(dispatch, state),
+    dispatch,
     saga: function* () {
       yield parallel(duck.sagas);
     },
@@ -62,3 +36,49 @@ export function useSagaDuckState<T extends DuckMap = any>(
     duck,
   };
 }
+
+const getCurrentTimeFormatted = () => {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+  const milliseconds = currentTime.getMilliseconds();
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
+
+const logger =
+  process.env.NODE_ENV === "development"
+    ? (reducer) => {
+        const reducerWithLogger = useCallback(
+          (state, action) => {
+            const next = reducer(state, action);
+            console.groupCollapsed(
+              `%cAction: %c${action.type} %cat ${getCurrentTimeFormatted()}`,
+              "color: black; font-weight: bold;",
+              "color: bl; font-weight: bold;",
+              "color: grey; font-weight: lighter;"
+            );
+            console.log(
+              "%cPrevious State:",
+              "color: #9E9E9E; font-weight: 700;",
+              state
+            );
+            console.log(
+              "%cAction:",
+              "color: #00A7F7; font-weight: 700;",
+              action
+            );
+            console.log(
+              "%cNext State:",
+              "color: #47B04B; font-weight: 700;",
+              next
+            );
+            console.groupEnd();
+            return next;
+          },
+          [reducer]
+        );
+
+        return reducerWithLogger;
+      }
+    : (reducer) => reducer;
